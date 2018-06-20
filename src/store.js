c0daed048d93968bd37563a96987cc5d349a70df
Dennis van Bennekom <vanbennekomdennis@gmail.com>
2018-06-20 09:16:03 +0200

    Update

--- src/store.js
+++ src/store.js
@@ -5,21 +5,43 @@ import Spotify from 'spotify-web-api-js';
 Vue.use(Vuex);
 
 const spotify = new Spotify();
-window.spotify = spotify;
 
 export default new Vuex.Store({
     state: {
-        accessToken: ''
+        accessToken: '',
+        loading: false,
+        playlists: [],
+        selected: null
     },
     mutations: {
-        setAccessToken (state, accessToken) {
+        accessToken(state, accessToken) {
             state.accessToken = accessToken;
             spotify.setAccessToken(accessToken);
+        },
+        loading(state, isLoading) {
+            state.loading = isLoading;
+        },
+        playlists(state, playlists) {
+            state.playlists = playlists;
+        },
+        select(state, playlist) {
+            state.selected = playlist;
         }
     },
     actions: {
-        setAccessToken ({ commit }, accessToken) {
-            commit('setAccessToken', accessToken);
+        setAccessToken({ commit }, accessToken) {
+            commit('accessToken', accessToken);
+        },
+        async getFeaturedPlaylists({ commit }) {
+            commit('loading', true);
+
+            const result = await spotify.getFeaturedPlaylists();
+
+            commit('playlists', result.playlists.items);
+            commit('loading', false);
+        },
+        select({ commit }, playlist) {
+            commit('select', playlist);
         }
     }
 });
--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,9 +1,38 @@
 <template>
-    <div>Hello now you can play!</div>
+    <div>
+        <ul>
+            <li v-for="playlist in playlists">
+                <a href="#" @click="select(playlist)">{{ playlist.name }}</a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
+    import { mapActions, mapState } from 'vuex';
+
     export default {
         name: 'play',
+        beforeRouteEnter(to, from, next) {
+            next(vm => {
+                if (!vm.$store.state.accessToken) {
+                    vm.$router.push({ name: 'home' });
+                }
+            });
+        },
+        computed: {
+            ...mapState({
+                playlists: state => state.playlists
+            })
+        },
+        mounted() {
+            this.getFeaturedPlaylists();
+        },
+        methods: {
+            ...mapActions([
+                'getFeaturedPlaylists',
+                'select'
+            ])
+        }
     }
 </script>
